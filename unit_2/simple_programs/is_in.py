9975616dac0b99d0360d1e3a31b3d89adb12ad67
mcshakes <efmccarthy26@gmail.com>
2021-02-10 17:25:14 -0700

    finished the recursive function

--- unit_2/simple_programs/is_in.py
+++ unit_2/simple_programs/is_in.py
@@ -1,8 +1,28 @@
-def isIn(char, aStr):
+def isIn(char: str, aStr: str) -> bool:
     '''
     char: a single character
     aStr: an alphabetized string
     
     returns: True if char is in aStr; False otherwise
     '''
-    # Your code here
+    if aStr == "":
+        return False
+    
+    if len(aStr) == 1:
+      return aStr == char
+
+    middle_idx = int(len(aStr) / 2) # after division, it's a float. so change to Int
+    sorted_str = "".join(sorted(aStr))
+
+    middle_char = sorted_str[middle_idx]
+    
+    if middle_char == char: 
+        return True
+    elif char < middle_char:
+        return isIn(char, sorted_str[:middle_idx])
+    else:
+        return isIn(char, sorted_str[middle_idx:])
+    
+
+
+# import code; code.interact(local=dict(globals(), **locals()))
