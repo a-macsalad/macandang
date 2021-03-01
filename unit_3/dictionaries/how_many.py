226efe31985fe1d5ea85d92ee085a38d1817df19
mcshakes <efmccarthy26@gmail.com>
2021-03-01 10:10:21 -0700

    Added exercises for Unit 3

--- unit_3/dictionaries/how_many.py
+++ unit_3/dictionaries/how_many.py
@@ -1,7 +1,16 @@
+animals = { 'a': ['aardvark'], 'b': ['baboon'], 'c': ['coati']}
+
 def how_many(aDict):
     '''
     aDict: A dictionary, where all the values are lists.
 
     returns: int, how many values are in the dictionary.
     '''
-    # Your Code Here
+    count = 0
+
+    for alp in aDict:
+        count = count + len(aDict[alp])
+    
+    print(count)
+
+how_many(animals)
