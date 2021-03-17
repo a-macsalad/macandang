173703921f9b8bc113ee90c8c28923a726eaee46
mcshakes <efmccarthy26@gmail.com>
2021-03-16 19:15:51 -0600

    Finito

--- unit_5/problem_set_5/mac/ps6/ps6.py
+++ unit_5/problem_set_5/mac/ps6/ps6.py
@@ -152,17 +152,17 @@ class CiphertextMessage(Message):
 
 
 #Example test case (Message)
-message = Message('we are taking 6.00.1x')
+# message = Message('we are taking 6.00.1x')
 # print('Expected Output: jgnnq')
-message.apply_shift(5)
+# message.apply_shift(5)
 
 
 #Example test case (PlaintextMessage)
-plaintext = PlaintextMessage('hello', 2)
-print('Expected Output: jgnnq')
-print('Actual Output:', plaintext.get_message_text_encrypted())
+# plaintext = PlaintextMessage('hello', 2)
+# print('Expected Output: jgnnq')
+# print('Actual Output:', plaintext.get_message_text_encrypted())
     
 #Example test case (CiphertextMessage)
-ciphertext = CiphertextMessage('jgnnq')
-print('Expected Output:', (24, 'hello'))
-print('Actual Output:', ciphertext.decrypt_message())
+# ciphertext = CiphertextMessage('jgnnq')
+# print('Expected Output:', (24, 'hello'))
+# print('Actual Output:', ciphertext.decrypt_message())
